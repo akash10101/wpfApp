fc2acb28341fbb1580d7f395bd543134b9e6e63a
akash10101 <70319947+akash10101@users.noreply.github.com>
2020-10-21 23:27:45 +0530

    commit

---
+++ WpfApp1/WpfApp1/Window1.xaml.cs
@@ -0,0 +1,57 @@
+﻿using System.Windows;
+using System.Net.Http;
+using AssistPurchase;
+using AssistPurchase.Models;
+using System.Text.Json.Serialization;
+using Newtonsoft.Json;
+using System.Text;
+
+namespace WpfApp1
+{
+    /// <summary>
+    /// Interaction logic for Window1.xaml
+    /// </summary>
+    public partial class Window1 : Window
+    {
+        
+        
+        private static readonly HttpClient client = new HttpClient();
+        public string fruits;
+        public Window1()
+        {
+            InitializeComponent();
+           
+           
+        }
+
+        private async System.Threading.Tasks.Task enter_ClickAsync(object sender, RoutedEventArgs e)
+        {
+            var newProduct = new MonitoringProducts()
+            {
+                ProductNumber = productNumber.Text,
+                ProductName = productName.Text,
+                ProductDescription = productDescp.Text,
+                TouchScreen = touchScreen.Text,
+                WearableMonitor = wearableMonitor.Text,
+                AlarmManagement = alarmManagement.Text,
+                Cost = cost.Text,
+                ScreenSize = screenSize.Text,
+                ConnectivitySupport = connec.Text,
+                SummarizeDataSupport = summerize.Text,
+                Compact = compact.Text,
+                ScalableMeasurement = scalable.Text
+
+            };
+
+            var response = await client.PostAsync("http://localhost:5000/api/MonitoringProduct" + "/new", new StringContent(JsonConvert.SerializeObject(newProduct), Encoding.UTF8, "application/json"));
+            var responseString = await response.Content.ReadAsStringAsync();
+
+            productNumber.Text = responseString;
+        }
+
+        private void enter_Click(object sender, RoutedEventArgs e)
+        {
+            var result= enter_ClickAsync(sender, e);
+        }
+    }
+}
